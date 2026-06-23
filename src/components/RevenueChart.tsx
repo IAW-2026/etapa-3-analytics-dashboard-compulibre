4ea44215e4e5aad10e63feef0954423a066875d6
JuanseGenca <juanssegenca@gmail.com>
2026-06-23 14:05:28 -0300

    fix bug de numeros

--- src/components/RevenueChart.tsx
+++ src/components/RevenueChart.tsx
@@ -5,13 +5,22 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContai
 export default function RevenueChart({ data }: { data: { date: string; revenue: number }[] }) {
   if (!data || data.length === 0) return <p className="text-gray-500 text-sm">No hay datos de ingresos.</p>;
 
+  const formatCompactNumber = (number: number) => {
+    return new Intl.NumberFormat('es-AR', { 
+      style: 'currency', 
+      currency: 'ARS', 
+      notation: "compact",
+      maximumFractionDigits: 1 
+    }).format(number);
+  };
+
   return (
     <div className="w-full h-full min-h-[300px]">
       <ResponsiveContainer width="100%" height="100%">
-        <LineChart data={data} margin={{ top: 10, right: 10, left: -20, bottom: 0 }}>
+        <LineChart data={data} margin={{ top: 10, right: 10, left: 10, bottom: 0 }}>
           <CartesianGrid strokeDasharray="3 3" vertical={false} stroke="#E5E7EB" />
           <XAxis dataKey="date" axisLine={false} tickLine={false} tick={{ fontSize: 12, fill: '#6B7280' }} dy={10} />
-          <YAxis axisLine={false} tickLine={false} tick={{ fontSize: 12, fill: '#6B7280' }} tickFormatter={(value) => `$${value}`} />
+          <YAxis axisLine={false} tickLine={false} tick={{ fontSize: 12, fill: '#6B7280' }} width={40} tickFormatter={formatCompactNumber}/>
           <Tooltip 
             formatter={(value: any) => {
                 // Nos aseguramos de tratarlo como número antes de formatearlo
